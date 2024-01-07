257eacfb8543459117f2e8fef99d96c71a3ef263
bea0266 <bea0266@naver.com>
2024-01-07 16:01:24 +0900

    로고, 메뉴 스타일 작성

--- src/views/components/MenuComp.jsx
+++ src/views/components/MenuComp.jsx
@@ -1,8 +1,14 @@
+import styles from '/src/main.module.scss';
 /** 메뉴 컴포넌트 */
 export default function MenuComp() {
     return (
       <>
-        <div>메뉴 컴포넌트</div>
+        <ul className={styles['menu-list']}>
+          <li className={styles['menu-item']}>공지사항</li>
+          <li className={styles['menu-item']}>커뮤니티</li>
+          <li className={styles['menu-item']}>자료실</li>
+          <li className={styles['menu-item']}>개발자 문의</li>
+        </ul>
       </>   
     );
 }
--- src/views/main/MainView.jsx
+++ src/views/main/MainView.jsx
@@ -1,11 +1,16 @@
-import MenuComp from "../components/MenuComp";
-
+import styles from '/src/main.module.scss';
+import MenuComp from '/src/views/components/MenuComp';
 /** 메인 화면 */
 export default function MainView() {
 	return (
-		<>
-			<h1>메인 화면</h1>
-			<MenuComp />
+		<>	
+			<div className={styles['main-header-container']}>
+				<img className={styles.logo} />
+				<div className={styles['menu-container']}>
+					<MenuComp />
+				</div>
+			</div> 
+
 		</>
 	);
 }
