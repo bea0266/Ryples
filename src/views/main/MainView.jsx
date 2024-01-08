d0a7843c4510f7a6b3f313955d9dd3243fdc3b95
bea0266 <bea0266@naver.com>
2024-01-09 00:33:10 +0900

    메뉴 레이아웃 수정, 검색창 추가

--- src/views/main/MainView.jsx
+++ src/views/main/MainView.jsx
@@ -1,16 +1,27 @@
-import styles from '/src/main.module.scss';
-import MenuComp from '/src/views/components/MenuComp';
+import styles from "/src/main.module.scss";
+import MenuComp from "/src/views/components/MenuComp";
 /** 메인 화면 */
 export default function MainView() {
 	return (
-		<>	
-			<div className={styles['main-header-container']}>
+		<>
+			<div className={styles["main-header-container"]}>
 				<img className={styles.logo} />
-				<div className={styles['menu-container']}>
+				<div className={styles["menu-container"]}>
 					<MenuComp />
 				</div>
-			</div> 
-
+				<div className={styles["btn-container"]}>
+					<button className={styles["btn-item"]}>
+						다크 모드 OFF
+					</button>
+					<button className={styles["btn-item"]}>설정</button>
+					<button className={styles["btn-item"]}>로그인</button>
+				</div>
+			</div>
+			<div className={styles["contents-container"]}>
+				<div className={styles["search-container"]}>
+					<input className={styles["search-input"]} placeholder="검색할 내용을 입력해주세요." />
+				</div>
+			</div>
 		</>
 	);
 }
